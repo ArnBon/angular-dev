3c02da2b8767322200c148380bc65d544aac5c50
ArnBon <bonillaarnaldo@gmail.com>
2024-02-20 19:34:16 -0400

    clase 20 1eros componentes e inicio de la estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { TestComponent } from './auth/test/test.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    TestComponent,
+    NoPageFoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
