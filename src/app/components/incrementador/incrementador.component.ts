838952b1f5b10df3737a550befd562a39ec194f3
ArnBon <bonillaarnaldo@gmail.com>
2024-02-26 17:40:41 -0400

    clase 53 output componente incrementador

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,41 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
-  styleUrls: ['./incrementador.component.css']
+  styleUrls: []
 })
 export class IncrementadorComponent  {
 
- progreso1: number = 25;
-    progreso2: number = 35;
-    progreso: number = 40;
-
-    get getProgreso1() {
-      return `${ this.progreso1 }%`;
-    }
-
-    get getProgreso2() {
-      return `${ this.progreso2 }%`;
-    }
-
-
-    get getPorcentaje() {
-      return `${ this.progreso }%`;
-    }
-
-
+    // @Input('valor') progreso: number = 40; RENOMBRAR EL ARGUMENTO
+    @Input('valorEntrada') progreso: number = 40;
+    @Output() valorSalida: EventEmitter<number> = new EventEmitter();
 
     cambiarValor(valor: number){
 
       if (this.progreso >= 100 && valor >= 0) {
+        this.valorSalida.emit(100);
         return this.progreso = 100;
       }
 
       if (this.progreso <= 0 && valor < 0 ) {
+        this.valorSalida.emit(0);
         return this.progreso = 0;
       }
-this.progreso = this.progreso + valor;
+      this.progreso = this.progreso + valor;
+      this.valorSalida.emit(this.progreso);
     }
-
 }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent  {
 
-    
+    progreso1: number = 25;
+    progreso2: number = 35;
 
 
+    get getProgreso1() {
+      return `${ this.progreso1 }%`;
+    }
 
+    get getProgreso2() {
+      return `${ this.progreso2 }%`;
+    }
 
+    cambioValorHijo(valor: number){
+      console.log('funciona!', valor);
+    }
 }
