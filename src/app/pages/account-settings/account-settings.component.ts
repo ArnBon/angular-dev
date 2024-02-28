6ccf321db90751a065208bf49a5462af6a228743
ArnBon <bonillaarnaldo@gmail.com>
2024-02-28 19:09:08 -0400

    clase 66 cambiar el css principal de forma dinamica

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
+  public linkTheme = document.querySelector('#theme');
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+   changeTheme( theme: string ) {
+
+    // console.log( theme );
+    const url = `./assets/css/colors/${theme}.css`;
+    // console.log(url);
+    // console.log( this.linkTheme );
+    this.linkTheme.setAttribute('href', url);
+    localStorage.setItem('theme', url);
   }
 
 }
