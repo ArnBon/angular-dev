4617e8b97d1fa1b9da59eda73be7be7edaeaba22
ArnBon <bonillaarnaldo@gmail.com>
2024-03-02 19:48:07 -0400

    clase 70 servicio para controlar sidebar menu lateral

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingService } from 'src/app/services/setting.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -10,39 +11,42 @@ export class AccountSettingsComponent implements OnInit {
   public linkTheme = document.querySelector('#theme');
   public links: NodeListOf<Element>;
 
-  constructor() { }
+  constructor(private settingService: SettingService) { }
 
   ngOnInit(): void {
-    this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTheme();
+    this.settingService.checkCurrentTheme();
+    // this.links = document.querySelectorAll('.selector');
+    // this.checkCurrentTheme();
   }
 
    changeTheme( theme: string ) {
 
     // console.log( theme );
-    const url = `./assets/css/colors/${theme}.css`;
+    // const url = `./assets/css/colors/${theme}.css`;
     // console.log(url);
     // console.log( this.linkTheme );
-    this.linkTheme.setAttribute('href', url);
-    localStorage.setItem('theme', url);
+    this.settingService.changeTheme(theme)
+    // this.linkTheme.setAttribute('href', url);
+    // localStorage.setItem('theme', url);
 
-    this.checkCurrentTheme();
+    // this.checkCurrentTheme();
+    // this.settingService.checkCurrentTheme();
   }
 
-  checkCurrentTheme() {
-    this.links.forEach( elem => {
+  // checkCurrentTheme() {
+  //   this.links.forEach( elem => {
 
-      elem.classList.remove('working');
-      const btnTheme = elem.getAttribute('data-theme');
-      const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
-      const currentTheme = this.linkTheme.getAttribute('href');
+  //     elem.classList.remove('working');
+  //     const btnTheme = elem.getAttribute('data-theme');
+  //     const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
+  //     const currentTheme = this.linkTheme.getAttribute('href');
 
-      if ( btnThemeUrl === currentTheme ) {
-        elem.classList.add('working');
-      }
+  //     if ( btnThemeUrl === currentTheme ) {
+  //       elem.classList.add('working');
+  //     }
 
-    });
+  //   });
 
-  }
+  // }
 
 }
