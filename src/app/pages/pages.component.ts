7d6c39f8b2b44986dc87ad9bc2233c150cb6e6d0
ArnBon <bonillaarnaldo@gmail.com>
2024-03-02 20:10:06 -0400

    clase 71 uso de scripts de archivos importados en el index.html

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingService } from '../services/setting.service';
 
+declare function customInitFunctions();
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -15,6 +16,8 @@ export class PagesComponent implements OnInit {
     // const url = localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
     // this.linkTheme.setAttribute('href', url);
     // localStorage.setItem('theme', url);
+
+    customInitFunctions();
 
   }
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -14,6 +14,7 @@ import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
 
 
 
@@ -25,6 +26,7 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     Grafica1Component,
     PagesComponent,
     AccountSettingsComponent,
+    PromesasComponent,
   ],
   exports: [
     DashboardComponent,
