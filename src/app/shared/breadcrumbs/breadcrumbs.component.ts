5efcaa500f6a759f1044e97548e6605e8a3b43ca
ArnBon <bonillaarnaldo@gmail.com>
2024-03-06 18:42:23 -0400

    clase Breadcrumbs usando observables y parametros de rutas

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,7 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  public titulo: string;
+  constructor(private router: Router) {
+  }
+  getArgumentosRuta() {
+    this.router.events
+  .pipe(
+    filter( event => event instanceof ActivationEnd ),
+    filter( ( event: ActivationEnd) => event.snapshot.firstChild === null ),
+    map((event: ActivationEnd) => event.snapshot.data),
+    )
+  .subscribe( ({ titulo }) => {
+    this.titulo = titulo
+    document.title = `AdminPro ${titulo}` ;
+  });
+  }
 
   ngOnInit(): void {
   }
