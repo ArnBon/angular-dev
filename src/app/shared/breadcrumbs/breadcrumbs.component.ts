ac491d15d076910b634d41180839d4c2b8e80ecb
ArnBon <bonillaarnaldo@gmail.com>
2024-03-07 18:15:30 -0400

    clase clase 88 optimizaciones del breadcrumb

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 
@@ -8,25 +9,33 @@ import { filter, map } from 'rxjs/operators';
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.css']
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
   public titulo: string;
+  public tituloSubs$: Subscription;
+
   constructor(private router: Router) {
-  }
-  getArgumentosRuta() {
-    this.router.events
-  .pipe(
-    filter( event => event instanceof ActivationEnd ),
-    filter( ( event: ActivationEnd) => event.snapshot.firstChild === null ),
-    map((event: ActivationEnd) => event.snapshot.data),
-    )
-  .subscribe( ({ titulo }) => {
+    this.tituloSubs$ = this.getArgumentosRuta()
+    .subscribe( ({ titulo }) => {
     this.titulo = titulo
     document.title = `AdminPro ${titulo}` ;
   });
   }
 
-  ngOnInit(): void {
-  }
+  getArgumentosRuta() {
+    return this.router.events
+    .pipe(
+      filter( event => event instanceof ActivationEnd ),
+      filter( ( event: ActivationEnd) => event.snapshot.firstChild === null ),
+      map((event: ActivationEnd) => event.snapshot.data),
+      )}
+
+
+      ngOnDestroy(): void {
+        this.tituloSubs$.unsubscribe();
+      }
+
+
+
 
 }
