73c0cb842be0fd1b8d723995c988b229dfa6cf55
ArnBon <bonillaarnaldo@gmail.com>
2024-04-08 19:15:15 -0400

    clases de la 164 a la 167 de la seccion 14

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -20,6 +20,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     RouterModule,
     FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
 
-  constructor() { }
+  public formSubmitted = false; //clase 167
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    nombre: ['',         Validators.required],
+    email: ['', Validators.required, Validators.email], // agregado clase 167 Validators.email
+    password: ['',        Validators.required],
+    password2: ['',       Validators.required],
+    terminos: [false,           Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  crearUsuarios(){
+    this.formSubmitted = true; //clase 167
+    console.log(this.registerForm.value);
+
+    if (this.registerForm.valid) {
+          console.log('posteando formulario')
+      } else {
+        console.log('Formulario no es correcto');
+    }
+  }
+
+  campoNoValido( campo: string ): boolean { //clase 167
+
+    if ( this.registerForm.get(campo).invalid && this.formSubmitted ) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  aceptaTerminos() { //clase 167
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
   }
 
 }
