dbb00da1496700e469a1e2b4096e6df45a7788e7
ArnBon <bonillaarnaldo@gmail.com>
2024-04-13 11:11:51 -0400

    clase 170 usuario Service crear usuario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
 
 @Component({
   selector: 'app-register',
@@ -11,25 +13,46 @@ export class RegisterComponent  {
   public formSubmitted = false; //clase 167
 
   public registerForm = this.fb.group({
-    nombre: ['',         Validators.required],
-    email: ['', Validators.required, Validators.email], // agregado clase 167 Validators.email
-    password: ['',        Validators.required],
-    password2: ['',       Validators.required],
-    terminos: [false,           Validators.required],
+    nombre:    ['',    Validators.required],
+    email:     ['',    Validators.required, Validators.email], // agregado clase 167 Validators.email
+    password:  ['',    Validators.required],
+    password2: ['',    Validators.required],
+    terminos:  [false, Validators.required],
+  }, {
+    validators: this.passwordsIguales('password', 'password2')
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    private usuarioService: UsuarioService
+  ) { }
 
+  // crearUsuarios(){
+  //   this.formSubmitted = true; //clase 167
+  //   console.log(this.registerForm.value);
+  //   console.log(this.registerForm);
+
+  //   if (this.registerForm.valid) {
+  //         console.log('posteando formulario')
+  //     } else {
+  //       console.log('Formulario no es correcto');
+  //   }
+  // }
+
+  //clase 170
   crearUsuarios(){
     this.formSubmitted = true; //clase 167
     console.log(this.registerForm.value);
-
-    if (this.registerForm.valid) {
-          console.log('posteando formulario')
-      } else {
-        console.log('Formulario no es correcto');
-    }
+    if (this.registerForm.invalid) {
+          return;
   }
+
+  //realizar posteo
+  this.usuarioService.crearUsuario(this.registerForm.value)
+  .subscribe(resp => {
+    console.log('usuario creado')
+    console.log(resp);
+  }, (err)=> console.warn(err.error.msg));
+}
 
   campoNoValido( campo: string ): boolean { //clase 167
 
@@ -41,8 +64,32 @@ export class RegisterComponent  {
 
   }
 
+  contrasenasNoValidas(){
+    const pass1 = this.registerForm.get('password').value;
+    const pass2 = this.registerForm.get('password2').value;
+
+    if (pass1 != pass2 && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
   aceptaTerminos() { //clase 167
     return !this.registerForm.get('terminos').value && this.formSubmitted;
   }
 
+  passwordsIguales(passName1: string, passName2: string){
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(passName1);
+      const pass2Control = formGroup.get(passName2);
+
+      if (pass1Control.value === pass2Control.value) {
+        pass2Control.setErrors(null)
+      } else {
+        pass2Control.setErrors({noEsigual: true})
+      }
+    }
+  }
 }
---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RegisterForm } from '../interfaces/register-form.interface';
+import { environment } from 'src/environments/environment';
+
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  crearUsuario(formData: RegisterForm){
+    // console.log('creando usuario');
+    return this.http.post(`${base_url}/usuarios`, formData);
+  }
+}
