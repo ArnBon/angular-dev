cf6a75bc64d814264c4f24768f38b7b5a31cd6ae
ArnBon <bonillaarnaldo@gmail.com>
2024-08-10 10:48:32 -0400

    clase 200 continuacion del proyecto adminPro

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -18,6 +18,16 @@ export class SidebarService {
         {titulo: 'Promesas', url:"promesas"},
         {titulo: 'Rxjs', url:"rxjs"},
       ]
+    },
+
+    {
+      titulo: 'Mantenimiento',
+      icono: 'mdi mdi-folder-lock-open',
+      submenu: [
+        {titulo: 'Usuarios', url: "usuarios"},
+        {titulo: 'Hospitales', url:"hospitales" },
+        {titulo: 'Médicos', url:"medicos"},
+      ]
     }
   ];
 }
